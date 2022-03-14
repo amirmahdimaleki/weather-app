32d59354a0d0961e8193df0c3613c2786b4f6886
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-14 20:21:07 +0330

    io

--- js/forcast.js
+++ js/forcast.js
@@ -4,7 +4,11 @@ const getWeather = async (id) => {
 
   const base = "http://dataservice.accuweather.com/currentconditions/v1/"
   const query = `${id}?apikey=${key} `
+  
+  const response = await fetch(base + query);
+  const data = await response.json();
 
+  console.log(data);
 }
 
 
@@ -14,12 +18,13 @@ const getCity =  async (city) =>{
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apikey=${key}&q=${city}`;
 
-    const response = await fetch(base + query)
+    const response = await fetch(base + query);
     const data = await response.json();
     return data[0];
 };
 
 
-getCity("tehran")
-.then(data =>console.log(data))
-.catch(err => console.log(err))
+// getCity("tehran")
+// .then(data =>console.log(data))
+// .catch(err => console.log(err))
+getWeather(329260)
