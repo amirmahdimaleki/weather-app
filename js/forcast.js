d2f5ae1a497987751a3153508cb5050a89c291f7
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-14 19:37:00 +0330

    data json

--- js/forcast.js
+++ js/forcast.js
@@ -3,9 +3,9 @@ const key = 'nbpPNlkAi6yQnSnHdL6VKQd7F6eWTLcE'
 const getCity =  async (city) =>{
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?apiKey=${key}&q=${city}`;
+    const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query)
-
+    const data = await response.json();
 
 };
