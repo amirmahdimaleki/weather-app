0e9043cc5e825c11aef4f535cbcac049c6e27758
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-16 21:07:52 +0330

    bit

--- js/forcast.js
+++ js/forcast.js
@@ -1,27 +1,27 @@
 const key = ' nbpPNlkAi6yQnSnHdL6VKQd7F6eWTLcE '
 // * weather getter func
 const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
 
-  const base = ' http://dataservice.accuweather.com/currentconditions/v1/ '
-  const query = ` ${id}?apikey=${key} `
-  
   const response = await fetch(base + query);
   const data = await response.json();
 
-  return data[0]
-}
-
-
-// * city getter func
-const getCity =  async (city) => {
-
-    const base = ' http://dataservice.accuweather.com/locations/v1/cities/search '
-    const query = ` ?apikey=${key}&q=${city} `;
-
-    const response = await fetch(base + query );
-    const data = await response.json();
-    return data[0];
+  return data[0];
 };
+
+// get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
 
 // * data getting
 // getCity( 'manchester' ).then( data => {
