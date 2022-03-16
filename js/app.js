62aef3405a0c5aa8061b2a7fc8fed594d04b7fe9
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-16 20:45:48 +0330

    dom

--- js/app.js
+++ js/app.js
@@ -2,6 +2,23 @@ const cityForm = document.querySelector('form');
  const card  = document.querySelector('.card');
  const details = document.querySelector('.details')
 
+const updateUI = (data) => {
+ 
+const cityDets = data.cityDets;
+const weather = data.weather;
+
+// update details template 
+details.innerHTML = `
+<h5 class="my-3">${cityDets.EnglishName}</h5>
+<div class="my=3">${weather.WeatherText}</div>
+<div class="display-4 my-4">
+  <span>${weather.Temperature.Metric}</span>
+  <span>&deg;C</span>
+</div>
+`
+     
+};
+
 const updateCity = async (city)=>{
     // console.log(city)
     const cityDets = await getCity(city);
@@ -19,6 +36,6 @@ cityForm.addEventListener('submit', e =>{
 
     // update the ui with new city
     updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
-});
+}
