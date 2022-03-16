001fb957cfb6907babde6e8d0b59a79e95f11f9b
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-16 20:59:29 +0330

    bit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-const cityForm = document.querySelector('form');
- const card  = document.querySelector('.card');
- const details = document.querySelector('.details')
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
 
 const updateUI = (data) => {
-        
-     const cityDets = data.cityDets;
-     const weather = data.weather;
+  const cityDets = data.cityDets;
+  const weather = data.weather;
 
-      // update details template 
-      details.innerHTML = `
+  //   update details template
+  details.innerHTML = `
     <h5 class="my-3">${cityDets.EnglishName}</h5>
           <div class="my=3">${weather.WeatherText}</div>
           <div class="display-4 my-4">
@@ -18,23 +17,23 @@ const updateUI = (data) => {
 `;
 };
 
-const updateCity = async (city)=>{
-    // console.log(city)
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
-    
-    return { cityDets , weather};
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return { cityDets, weather };
 };
 
-cityForm.addEventListener('submit', e =>{
-    e.preventDefault();
-    
-    // get cityValue
-    const city = cityForm.city.value.trim();
-    cityForm.reset();
+cityForm.addEventListener("submit", (e) => {
+  // prevent defult action
+  e.preventDefault();
 
-    // update the ui with new city
-    updateCity(city)
-    .then(data => updateUI(data))
-    .catch(err => console.log(err));
+  //  get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  //  update the ui with new city
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 });
