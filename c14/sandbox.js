f1673b66eb123d0b3dffd8c9d037bce95f432ef0
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-21 14:23:51 +0330

    localStorage

---
+++ c14/sandbox.js
@@ -0,0 +1,23 @@
+// ! LOCAL STORAGE EXAMPLES
+
+//  store data in local storage
+
+localStorage.setItem('name1', 'amir')
+localStorage.setItem('age', 17)
+
+// ? keys and values in local storage always turn into string => 17 > "17"
+// ? now the data is stored in my browser so if I comment 5&6 it will work too.
+
+//  get data from local storage
+let name1 = localStorage.getItem('name1')
+let age = localStorage.getItem('age')
+
+console.log(name1, age)
+
+//  updating data
+
+// ? we update data by using setItem and overwriting it
+
+localStorage.setItem('name1', 'amirmahdi')
+
+name1 = localStorage.getItem('name1')
